21c1bb4060ba6ed37097bc2eb22af23e3376e42c
Greg Zynda <gzynda@login1.ls4.tacc.utexas.edu>
2015-02-24 22:25:56 -0600

    added segmentation, but bams are sorted in reference order instead of numerical order

--- logfold_rep.py
+++ logfold_rep.py
@@ -5,14 +5,18 @@ import math
 import argparse
 import numpy as np
 from multiprocessing import Pool, cpu_count
+from glob import glob
+
+myColors = ("#85BEFF", "#986300", "#009863", "#F2EC00", "#F23600", "#C21BFF", "#85FFC7")
+colorDict = {frozenset([0]):myColors[0], frozenset([1]):myColors[1], frozenset([2]):myColors[2], frozenset([2,1]):myColors[3], frozenset([2,0]):myColors[4], frozenset([1,0]):myColors[5], frozenset([2,1,0]):myColors[6]}
+zero = 0.0004
 
 def run_logFC(fList):
 	beds = map(lambda y: y[1]+".bedgraph", fList)
 	print len(beds), beds
 	vals = []
 	print "Parsing Files"
-	locations = processFiles(vals, beds)
-	vals = np.array(vals)
+	locations, vals = processFiles(beds)
 	normVals = normalize(vals)
 	del vals
 	for bIndex in xrange(1,len(beds)):
@@ -28,7 +32,7 @@ def run_logFC(fList):
 			OF.close()
 
 def smooth(level, fList, chromDict):
-	beds = map(lambda y: y[1]+"_logFC.bedgraph", fList)
+	beds = map(lambda y: y[1]+"_logFC.bedgraph", fList[1:])
 	print "Smoothing:",beds
 	for bed in beds:
 		outFile = '%s_%i.smooth.bedgraph'%(bed.rstrip('.bedgraph'),level)
@@ -36,7 +40,7 @@ def smooth(level, fList, chromDict):
 		for chr in chromDict:
 			os.system('grep "^%s\t" %s | cut -f 4 > tmp.val' % (chr, bed))
 			os.system('grep "^%s\t" %s | cut -f 1-3 > tmp.loc'%(chr, bed))
-			os.system('./wavelets --level %i --to-stdout --boundary reflected --filter Haar tmp.val > tmp.smooth'%(level))
+			os.system('wavelets --level %i --to-stdout --boundary reflected --filter Haar tmp.val > tmp.smooth'%(level))
 			os.system('paste tmp.loc tmp.smooth >> %s'%(outFile))
 	os.system('rm tmp.val tmp.loc tmp.smooth')
 
@@ -90,18 +94,19 @@ def normalize(vals):
 	sf = sizeFactors(vals)
 	return np.array(vals/np.matrix(sf).T)
 
-def processFiles(vals, files):
+def processFiles(files):
+	vals = []
 	p = Pool(cpu_count())
 	for l,v in p.map(parseBG, files):
 		vals.append(v)
-	return l
+	return l, np.array(vals)
 
 def parseBG(inFile):
 	lines = open(inFile,'r').readlines()
 	tmp = map(lambda y: y.rstrip('\n').split('\t'), lines)
 	locs = map(lambda y: y[:3], tmp)
 	vals = np.array(map(lambda y: np.float(y[3]), tmp))
-	vals[vals < 2] = 0
+	#vals[vals < 2] = 0
 	return locs, vals
 
 def readFAI(inFile):
@@ -112,7 +117,8 @@ def readFAI(inFile):
 	chromDict = {}
 	for line in open(inFile,'r'):
 		tmp = line.split('\t')
-		chromDict[tmp[0]] = tuple(map(int, tmp[1:]))
+		#chromDict[tmp[0]] = tuple(map(int, tmp[1:]))
+		chromDict[tmp[0]] = int(tmp[1])
 	return chromDict
 
 def parseIN(inFile):
@@ -151,10 +157,162 @@ def main():
 	fList = parseIN(args.infile)
 	makeBedgraph(fList, args.F, args.S)
 	chromDict = readFAI(fai)
-	run_logFC(fList)
+	#run_logFC(fList)
+	#for i in range(1,args.L+1):
+	#	smooth(i, fList, chromDict)
+	makeGFF(fList, chromDict, args.L, args.S)
 	sys.exit()
-	for i in range(1,args.L+1):
-		smooth(i, fList, chromDict)
+
+def parseLocations(locations):
+	ctmp = ""
+	start = 0
+	locDict = {}
+	for i in xrange(len(locations)):
+		chrom = locations[i][0]
+		if chrom != ctmp:
+			if ctmp != "":
+				locDict[ctmp] = (start, i)
+			ctmp = chrom
+			start = i
+	locDict[ctmp] = (start,i+1)
+	return locDict
+
+def makeGFF(fList, chromDict, level, S):
+	sortedChroms = sorted(chromDict.keys()[:])
+	beds = map(lambda y: "%s_logFC_%i.smooth.bedgraph"%(y[1],level), fList[1:])
+	names = map(lambda y: y[1], fList[1:])
+	for name in names:
+		outGFF = '%s_logFC_%i.smooth.gff3'%(name,level)
+		open(outGFF,'w').write('##gff-version 3\n#track name="%s LogFold %ibp" gffTags=on\n' % (name,S))
+		open("logFC_segmentation.gff3",'w').write('##gff-version 3\n#track name="Segmentation %ibp" gffTags=on\n'%(S))
+	print "Parsing :",beds
+	locations, vals = processFiles(beds)
+	print "Done"
+	locDict = parseLocations(locations)
+	for chrom in sortedChroms:
+		s,e = locDict[chrom]
+		print chrom
+		tmpLoc = locations[s:e]
+		maskM = np.zeros((len(names),len(tmpLoc)),dtype=np.bool)
+		for i in xrange(len(beds)):
+			outGFF = '%s_logFC_%i.smooth.gff3'%(names[i],level)
+			outSignal = vals[i,s:e]
+			bMask = np.zeros(len(outSignal), dtype=np.bool)
+			bMask[outSignal > zero] = 1
+			maskM[i,:]=bMask[:]
+			rBounds = calcRegionBounds(bMask)
+			OF = open(outGFF,'a')
+			for rS,rE in rBounds:
+				OF.write("%s\t.\tgene\t%i\t%s\t.\t.\t.\tcolor=%s;\n" % (tmpLoc[rS][0], int(tmpLoc[rS][1])+1, tmpLoc[rE][2], myColors[i]))
+				#OF.write("%s\t.\tgene\t%i\t%s\t.\t.\t.\tID=gene%04d;color=%s;\n" % (locations[i][s][0], int(locations[i][s][1])+1, locations[i][e][2], count, myColors[i]))
+			OF.close()
+		OS = open('logFC_segmentation.gff3','a')
+		setSigI = set(range(3))
+		for s in powerSet(range(3)):
+			setS = set(s)
+			tA = np.ones(maskM.shape[1], dtype=np.bool)
+			for i in s: #intersection
+				tA = np.logical_and(maskM[i,:], tA)
+			for i in list(setSigI-setS): #remove differences
+				tA[maskM[i,:]] = 0
+			rBounds = calcRegionBounds(tA)
+			name = ''.join(map(lambda y: names[y],sorted(s)))
+			for rS,rE in rBounds:
+				OS.write("%s\t.\tgene\t%i\t%s\t.\t.\t.\tName=%s;color=%s;\n" % (tmpLoc[rS][0], int(tmpLoc[rS][1])+1, tmpLoc[rE][2], name, colorDict[frozenset(s)]))
+		OS.close()
+
+def powerSet(L):
+	'''
+	Removes the empty list from powerset
+
+	>>> powerSet([1,2])
+	[[1], [0], [1, 0]]
+	'''
+	def powerHelp(A):
+		'''
+		Builds powerset recursively.
+
+		>>> powerHelp([1,2])
+		[[], [1], [0], [1, 0]]
+		'''
+		if not A:
+			return [[]]
+		ret = powerHelp(A[1:])
+		return ret+[i+[A[0]] for i in ret]
+	return powerHelp(L)[1:]
+
+def haarVec(vals, p=80):
+	hC = pywt.wavedec(vals,'haar')
+	cutVal = np.percentile(np.abs(np.concatenate(hC)), p)
+	for A in hC:
+		A[np.abs(A) < cutVal] = 0
+	tVals = pywt.waverec(hC,'haar')
+	return tVals[:len(vals)]
+
+def hannVec(vals, windowSize=20):
+	# Smooths using a hanning window
+	w = np.hanning(windowSize)
+	return np.convolve(w/w.sum(), vals, mode='same')
+
+def mergeRegions(counter, distThresh=0):
+	'''
+	Merges regions that are closer than (<) the distance threshold.
+
+	Parameters
+	=============================
+	counter		Binary counter array
+	distThresh	Max distance threshold
+
+	>>> A=np.array([1,1,0,0,0,1,1])
+	>>> mergeRegions(A)
+	>>> A
+	array([1, 1, 0, 0, 0, 1, 1])
+	>>> mergeRegions(A, distThresh=3)
+	>>> A
+	array([1, 1, 0, 0, 0, 1, 1])
+	>>> mergeRegions(A, distThresh=4)
+	>>> A
+	array([1, 1, 1, 1, 1, 1, 1])
+	'''
+	bounds = calcRegionBounds(counter)
+	for i in xrange(len(bounds)-1):
+		start0, end0 = bounds[i]
+		start1, end1 = bounds[i+1]
+		if start1-end0-1 < distThresh:
+			counter[start0:end1] = 1
+def calcRegionBounds(counter):
+	'''
+	Returns the new lower and upper bounds over overlapped regions.
+
+	Parameters
+	=============================
+	counter		Binary counter array
+
+	>>> calcRegionBounds(np.array([1,1,0,0,1,1,1,0,0,1,1]))
+	array([[ 0,  1],
+	       [ 4,  6],
+	       [ 9, 10]])
+	'''
+	d = np.diff(counter)
+	idx, = d.nonzero()
+	if counter[0]:
+		idx = np.r_[-1, idx]
+	if counter[-1]:
+		idx = np.r_[idx, counter.size-1]
+	idx.shape = (-1,2)
+	idx[:,0] += 1
+	return idx
+
+#def parseBG(inFile,chrom):
+#	lines = open(inFile,'r').readlines()
+#	tmp = map(lambda y: y.rstrip('\n').split('\t'), lines)
+#	toKeep = []
+#	for line in tmp:
+#		if line[0] == chrom:
+#			toKeep.append(line)
+#	locs = map(lambda y: y[:3], toKeep)
+#	vals = np.array(map(lambda y: np.float(y[3]), toKeep))
+#	return locs, vals
 
 if __name__ == "__main__":
 	main()
